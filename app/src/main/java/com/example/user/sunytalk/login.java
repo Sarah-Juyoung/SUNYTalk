06842948c91b1a67a01f9a4d6bdc2228ee39e4c0
Sarah-Juyoung <jusara1004@gmail.com>
2018-04-30 13:41:42 -0500

    Merge branch 'master' of C:\Users\user\Desktop\SUNYTalk with conflicts.

--- app/src/main/java/com/example/user/sunytalk/login.java
+++ app/src/main/java/com/example/user/sunytalk/login.java
@@ -4,9 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class login extends AppCompatActivity{
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
+
 
 }
--- app/src/main/java/com/example/user/sunytalk/signup.java
+++ app/src/main/java/com/example/user/sunytalk/signup.java
@@ -1,4 +1,18 @@
 package com.example.user.sunytalk;
 
-public class signup {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class signup extends AppCompatActivity {
+
+    private static final int PER_LOGIN =1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        //start Login
+        startActivityForResult(AuthUI.getInsatance().createSignInIntentBuilder());
+    }
+
 }
