e948ed2e912e98d68a407531c4497938bc4b12a7
Jinwoo Choi <cjw9405@gmail.com>
2018-06-01 00:03:19 +0900

    anf

--- app/src/main/java/com/example/user/sunytalk/announcement.java
+++ app/src/main/java/com/example/user/sunytalk/announcement.java
@@ -2,12 +2,35 @@ package com.example.user.sunytalk;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class announcement extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    public void onBackPressed(){
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
 
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_announcement);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com/");
+
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
     }
 }
