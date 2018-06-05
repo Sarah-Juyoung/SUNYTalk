9d62a788621c816e7e62e0ae6e97250fd78a2aa4
Jinwoo Choi <cjw9405@gmail.com>
2018-06-05 15:35:30 +0900

    anf

--- app/src/main/java/com/example/user/sunytalk/setting.java
+++ app/src/main/java/com/example/user/sunytalk/setting.java
@@ -2,12 +2,37 @@ package com.example.user.sunytalk;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class setting extends AppCompatActivity {
 
 
+    private WebView webView;
+
+    @Override
+    public void onBackPressed(){
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+
+
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        setContentView(R.layout.activity_announcement);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://blackboard.stonybrook.edu");
+
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
     }
 }
